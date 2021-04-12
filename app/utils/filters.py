01ccb3052410fadb877748ecd611fe1a8485f4f8
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2021-04-12 09:00:17 -0700

    Fixed absolute url in post-info.html by addin // and a filter in format url to remove all protocal info

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -6,7 +6,7 @@ def format_date(date):
     return date.strftime('%m/%d/%y')
 
 def format_url(url):
-    return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+    return url.replace('http://', '').replace('https://', '').replace('www.', '').replace('//', '').split('/')[0].split('?')[0]
 
 def format_plural(amount,word):
     if amount != 1:
